cbe07279d7974928593a5d8275155201f7e369a1
Lucas Boscatti <lucas.boscatti@e-core.com>
2024-12-05 16:13:51 -0300

    schema for products, oders and orderdetails table and a json with a list of products

---
+++ database/db/schemas.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS Products (
+    ProductId INTEGER PRIMARY KEY AUTOINCREMENT,
+    ProductName TEXT NOT NULL,
+    Description TEXT,
+    Price REAL NOT NULL CHECK(Price > 0),
+    Quantity INTEGER NOT NULL CHECK(Quantity >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    OrderId INTEGER PRIMARY KEY AUTOINCREMENT,
+    CustomerId INTEGER NOT NULL,
+    OrderDate TEXT NOT NULL,
+    Status TEXT NOT NULL CHECK(Status IN ('Pending', 'Shipped', 'Cancelled', 'Completed')),
+    FOREIGN KEY (CustomerId) REFERENCES Customers (CustomerId)
+);
+
+CREATE TABLE IF NOT EXISTS OrderDetails (
+    OrderDetailId INTEGER PRIMARY KEY AUTOINCREMENT,
+    OrderId INTEGER NOT NULL,
+    ProductId INTEGER NOT NULL,
+    Quantity INTEGER NOT NULL CHECK(Quantity > 0),
+    UnitPrice REAL NOT NULL CHECK(UnitPrice > 0),
+    FOREIGN KEY (OrderId) REFERENCES Orders (OrderId),
+    FOREIGN KEY (ProductId) REFERENCES Products (ProductId)
+);
